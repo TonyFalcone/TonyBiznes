e42d706cb0347bb8058e727817c8914d010d9bed
TonyFalcone <pozerek1998@wp.pl>
2026-06-29 21:10:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,16 @@
 // TONYBIZNES
 // APP.JS CZĘŚĆ 1
 // =========================
+const SUPABASE_URL =
+"https://tonyfalcone.github.io/TonyBiznes/";
 
+const SUPABASE_KEY =
+"sb_publishable_Y334pcLp_Jj3pzaeOmO_SA__jWp7RaD";
+
+const supabase =
+window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
 const COSTS = {
 
     seed: 2800,
